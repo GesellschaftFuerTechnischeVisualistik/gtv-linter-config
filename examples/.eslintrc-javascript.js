68d0b05f04d5593b9eca2942593011bdfbc3ad93
Andreas Stahl <andreas.bjorn.stahl@gmail.com>
2021-01-11 16:53:00 +0100

    Create .eslintrc-javascript.js

---
+++ examples/.eslintrc-javascript.js
@@ -0,0 +1,20 @@
+// rename this file to '.eslintrc.js' and put it in the project root
+module.exports = {
+  root: true,
+  env: {
+    'browser': true,
+    'node': true,
+    'es2020': true,
+    'jasmine': true
+  },
+  extends: [
+    '@gtvmbh/eslint-config
+    '@gtvmbh/eslint-config/eslint-config-jsdoc'
+  ],
+  ignorePatterns: [
+    '.eslintrc.js'
+  ],
+  rules: {
+    // add custom per-project rules here
+  }
+};
